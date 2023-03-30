55929160c5f7dd6810ca9ebdd53e6dd5eb2c8836
yunusemresenturk <emreesentrk@outlook.com>
2023-03-30 15:12:05 +0300

    d

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createRoot} from 'react-dom/client';
+import Card from './components/Card';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <div>
+      <div className='card-group'>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        <Card
+          cardTitle="Card Title1 "
+          cardText="Lorem İpsum Text 1"
+          updatedTime="Last updated 1 min ago"
+          image="https://picsum.photos/id/1/200/300"
+        />
+
+        <Card
+          cardTitle="Card Title1 "
+          cardText="Lorem İpsum Text 2"
+          updatedTime="Last updated 2 min ago"
+          image="https://picsum.photos/id/1/200/300"
+        />
+        <Card
+          cardTitle="Card Title1 "
+          cardText="Lorem İpsum Text 3"
+          updatedTime="Last updated 3 min ago"
+          image="https://picsum.photos/id/1/200/300"
+        />
+
+      </div>
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
